ef647cc9c72de2d5c076cce9ae81ff2b9efdfa1f
Sh1n3zZ <DengJunHai@foxmail.com>
2025-01-04 23:09:30 +0800

    chore: update ESLint configuration and modify build script to disable linting during build process

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,14 @@
 module.exports = {
+  extends: ['next/core-web-vitals'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  root: true,
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
-  },
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'react-hooks/rules-of-hooks': 'off'
+  }
 };
